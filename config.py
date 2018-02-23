53ee9a6055a0d7ab916af024802b3516644b2f38
taniaunid <36333822+taniaunid@users.noreply.github.com>
2018-02-22 21:34:48 -0600

    sin espacio

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import web
 
-db_host = '	i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_host = 'i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 db_name = 'lz6gjcwp6tqvj9c2'
 db_user = 'nv1ud6xcdpqyv2j2'
 db_pw = 'exyy8c4878431qq0'
